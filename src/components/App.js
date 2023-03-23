5c6fe25fcc708535d03f981a947525b2f47cbf89
sumit0593 <sumit0593>
2023-03-23 19:57:27 +0530

    new

--- src/components/App.js
+++ src/components/App.js
@@ -5,28 +5,58 @@ const App = () => {
 
   const [users, setUsers] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [sortAscending, setSortAscending] = useState(true);  
+  const [sortAscending, setSortAscending] = useState(true);
 
-  return (
+  //new 
+   const fetchData = async () => {
+    setIsLoading(true);
+    const response = await fetch(
+      'https://content.newtonschool.co/v1/pr/main/users'
+    );
+    const data = await response.json();
+    setIsLoading(false);
+    setUsers(data);
+  };
+
+  const sortUsers = () => {
+    setSortAscending(!sortAscending);
+    const sortedUsers = [...users].sort((a, b) =>
+      sortAscending
+        ? a.name.length - b.name.length
+        : b.name.length - a.name.length
+    );
+    setUsers(sortedUsers);
+  };
+  //
+
+   return (
     <div id="main">
       <h2>User List</h2>
-      <button className="fetch-data-btn">Fetch User Data</button>
-      <button className="sort-btn">
-        "Sort by name length (ascending)"
-        "Sort by name length (descending)"
+      <button className="fetch-data-btn" onClick={fetchData}>
+        Fetch User Data
       </button>
-      <p>Loading...</p>
-      <div className='users-section'>
-          <li>
-            <section className='id-section'></section>
-            <section className='name-email-section'>
-              <p className='name'>Name: </p>
-              <p className='email'>Email: </p>
-            </section>
-          </li>
-      </div>
+      <button className="sort-btn" onClick={sortUsers}>
+        {sortAscending
+          ? 'Sort by name length (ascending)'
+          : 'Sort by name length (descending)'}
+      </button>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <div className="users-section">
+          {users.map((user) => (
+            <li key={user.id}>
+              <section className="id-section">{user.id}</section>
+              <section className="name-email-section">
+                <p className="name">Name: {user.name}</p>
+                <p className="email">Email: {user.email}</p>
+              </section>
+            </li>
+          ))}
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
 
